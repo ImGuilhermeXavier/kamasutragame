7ef5e5e84382318b9be5a6183a0eee02496422e5
Guilherme Caçador <gxavier@esapiens.com.br>
2020-09-14 20:38:46 -0300

    .

--- src/positions.js
+++ src/positions.js
@@ -2,7 +2,197 @@ const steps = [
     {
         title: 'Caranguejo',
         description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-9.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
         image: require('./Static/Positions/ks-position-10.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-11.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-13.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-14.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-16.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-17.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-20.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-22.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-23.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-24.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-25.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-26.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-27.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-28.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-29.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-30.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-31.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-32.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-33.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-35.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-36.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-37.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-38.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-39.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-40.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-41.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-42.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-43.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-44.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-46.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-47.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-56.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-57.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-58.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-59.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-60.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-61.svg'),
+    },
+    {
+        title: 'Caranguejo',
+        description: 'Voce pode pular se quiser',
+        image: require('./Static/Positions/ks-position-62.svg'),
     },
 ];
 
